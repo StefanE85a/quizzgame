22a4ff96738c761d5ae8b3c0df3d7f5c81144429
StefanE85a <>
2023-12-29 22:26:27 +0000

    Added my test-questions/answers to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// The Questions and Answers
+const quizQuestions = [
+    {
+        question: "Which company produces the 'Model S' electric car?",
+        options: ["Nissan", "BMW", "Tesla", "Ford"],
+        answer: "Tesla"
+    },
+    {
+        question: "What does 'BMW' stand for?",
+        options: ["Bavarian Motor Works", "British Motor Works", "Berlin Motor Works", "Brussels Motor Works"],
+        answer: "Bavarian Motor Works"
+    },
+    {
+        question: "In what year was the first modern car invented?",
+        options: ["1886", "1901", "1879", "1895"],
+        answer: "1886"
+    },
+    {
+        question: "Which car is known as the first mass-produced vehicle?",
+        options: ["Ford Model T", "Chevrolet Impala", "Volkswagen Beetle", "Honda Civic"],
+        answer: "Ford Model T"
+    },
+    {
+        question: "What does the term 'SUV' stand for?",
+        options: ["Sport Utility Vehicle", "Special Utility Van", "Superior Urban Vehicle", "Speedy Urban Van"],
+        answer: "Sport Utility Vehicle"
+    },
+    {
+        question: "Which country is Lamborghini originally from?",
+        options: ["Italy", "Germany", "United States", "France"],
+        answer: "Italy"
+    },
+    {
+        question: "What is the main purpose of antilock braking system (ABS) in cars?",
+        options: ["Increase speed", "Decrease fuel consumption", "Prevent skidding during braking", "Cool down the engine"],
+        answer: "Prevent skidding during braking"
+    },
+    {
+        question: "Which company is known for producing the '911' model?",
+        options: ["Mercedes-Benz", "Audi", "Porsche", "Ferrari"],
+        answer: "Porsche"
+    },
+    {
+        question: "In which country was the car brand Volvo founded?",
+        options: ["Sweden", "Denmark", "Norway", "Finland"],
+        answer: "Sweden"
+    },
+    {
+        question: "What is the unit 'horsepower' used to measure?",
+        options: ["Engine efficiency", "Fuel consumption", "Engine power", "Speed"],
+        answer: "Engine power"
+    }
+];
+
